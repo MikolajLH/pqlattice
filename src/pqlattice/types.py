8e831315c6b2e34242d879c7be0a41612ec9bd6c
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-08-06 23:12:08 +0200

    declare numpy type aliases, can change in the future

---
+++ src/pqlattice/types.py
@@ -0,0 +1,14 @@
+import numpy as np
+from numpy.typing import NDArray
+
+type Vector = NDArray[np.float64 | np.int64]
+type Matrix = NDArray[np.float64 | np.int64]
+type SquareMatrix = NDArray[np.float64 | np.int64]
+
+type VectorFloat = NDArray[np.float64]
+type MatrixFloat = NDArray[np.float64]
+type SquareMatrixFloat = NDArray[np.float64]
+
+type VectorInt = NDArray[np.int64]
+type MatrixInt = NDArray[np.int64]
+type SquareMatrixInt = NDArray[np.int64]
