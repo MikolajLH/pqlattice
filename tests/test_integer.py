0414f4d2c5dc6ac2cbea55716219bdb9ca8887d5
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-08-08 23:52:19 +0200

    random tests for eaa

--- tests/test_integer.py
+++ tests/test_integer.py
@@ -1,11 +1,19 @@
 import math
+import random
+
+import pytest
 
 from pqlattice.integer.integer import eea
 
 
-def test_eea():
-    # TODO: use actual pytest features, parameterize instead of using hard coded values
-    a, b = 240, 46
+def make_randint_gen(a: int, b: int, n: int):
+    for _ in range(n):
+        yield random.randint(a, b)
+
+
+@pytest.mark.parametrize(
+    "a, b", zip(make_randint_gen(-(2**63), 2**63 - 1, 100), make_randint_gen(-(2**63), 2**63 - 1, 100), strict=False)
+)
+def test_eea(a: int, b: int):
     gcd, s, t = eea(a, b)
-    assert gcd == math.gcd(a, b)
-    assert a * s + b * t == gcd
+    assert gcd == math.gcd(a, b) and a * s + b * t == gcd
