251976f42b0f14093d00ff57ecab10ee17e0c685
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-08-09 23:53:39 +0200

    test exception throwing, add test for edge cases, group tests in one class

--- tests/test_integer.py
+++ tests/test_integer.py
@@ -11,9 +11,31 @@ def make_randint_gen(a: int, b: int, n: int):
         yield random.randint(a, b)
 
 
-@pytest.mark.parametrize(
-    "a, b", zip(make_randint_gen(-(2**63), 2**63 - 1, 100), make_randint_gen(-(2**63), 2**63 - 1, 100), strict=False)
-)
-def test_eea(a: int, b: int):
-    gcd, s, t = eea(a, b)
-    assert gcd == math.gcd(a, b) and a * s + b * t == gcd
+class Testeea:
+    # TODO: this mark.parametrize is kind of ugly, consider changing it
+    @pytest.mark.parametrize(
+        "a, b",
+        zip(make_randint_gen(-(2**63), 2**63 - 1, 100), make_randint_gen(-(2**63), 2**63 - 1, 100), strict=False),
+    )
+    def test_random(self, a: int, b: int):
+        gcd, s, t = eea(a, b)
+        assert gcd == math.gcd(a, b) and a * s + b * t == gcd
+
+    # TODO: replace hardcoded values that have no meaning (8 and 20) with fixtures that return random value
+    @pytest.mark.parametrize(
+        "inp, exp",
+        [
+            ((0, 8), (8, 0, 1)),
+            ((8, 0), (8, 1, 0)),
+            ((1, 1), (1, 0, 1)),
+            ((1, 20), (1, 1, 0)),
+            ((20, 1), (1, 0, 1)),
+            ((-1, 1), (1, 0, 1)),
+        ],
+    )
+    def test_cases(self, inp: tuple[int, int], exp: tuple[int, int, int]):
+        assert eea(*inp) == exp
+
+    def test_invalid(self):
+        with pytest.raises(ValueError):
+            eea(0, 0)
