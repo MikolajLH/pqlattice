0ea6f9bcf42010fc7cfac96518d10ca786ff8890
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-08-11 23:20:44 +0200

    write modpow function

--- src/pqlattice/integer/modring.py
+++ src/pqlattice/integer/modring.py
@@ -7,3 +7,16 @@ def modinv(a: int, modulus: int) -> int:
         raise ValueError(f"Modular inverse of {a} mod {modulus} does not exist; gcd is equal to {gcd}")
 
     return a_inv % modulus
+
+
+def modpow(a: int, r: int, modulus: int) -> int:
+    if r < 0:
+        return modpow(modinv(a, modulus), -r, modulus)
+
+    y, z = 1, a
+    while r != 0:
+        if r % 2 == 1:
+            y = (y * z) % modulus
+        r //= 2
+        z = (z * z) % modulus
+    return y
