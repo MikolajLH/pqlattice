e1e4d16d2def70e6401abaff7644473d3878997f
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-08-12 23:56:20 +0200

    start writing basic unit tests for modpow

--- tests/test_modring.py
+++ tests/test_modring.py
@@ -21,3 +21,22 @@ import pqlattice.integer.modring as mr
 )
 def test_modinv(inp: tuple[int, int], exp: int):
     assert mr.modinv(*inp) == exp
+
+
+@pytest.mark.parametrize(
+    "inp, exp",
+    [
+        ((1, 0, 2), 1),
+        ((1, 1, 2), 1),
+        ((1, 2, 2), 1),
+        ((1, 3, 2), 1),
+        ((2, 0, 5), 1),
+        ((2, 1, 5), 2),
+        ((2, 2, 5), 4),
+        ((2, 3, 5), 3),
+        ((2, 4, 5), 1),
+        ((2, 5, 5), 2),
+    ],
+)
+def test_modpow(inp: tuple[int, int, int], exp: int):
+    assert mr.modpow(*inp) == exp
