044d50548b986bccb90d4901ec2603a5b7a5e487
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-08-13 23:06:13 +0200

    write tests for modinv using random fixture

--- tests/test_modring.py
+++ tests/test_modring.py
@@ -1,42 +1,19 @@
+from collections.abc import Callable
+
 import pytest
 
 import pqlattice.integer.modring as mr
 
-
-@pytest.mark.parametrize(
-    "inp, exp",
-    [
-        ((1, 2), 1),
-        ((3, 2), 1),
-        ((11, 2), 1),
-        ((1, 3), 1),
-        ((4, 3), 1),
-        ((2, 3), 2),
-        ((1, 5), 1),
-        ((6, 5), 1),
-        ((2, 5), 3),
-        ((3, 5), 2),
-        ((4, 5), 4),
-    ],
-)
-def test_modinv(inp: tuple[int, int], exp: int):
-    assert mr.modinv(*inp) == exp
+from .conftest import PRIMES
 
 
-@pytest.mark.parametrize(
-    "inp, exp",
-    [
-        ((1, 0, 2), 1),
-        ((1, 1, 2), 1),
-        ((1, 2, 2), 1),
-        ((1, 3, 2), 1),
-        ((2, 0, 5), 1),
-        ((2, 1, 5), 2),
-        ((2, 2, 5), 4),
-        ((2, 3, 5), 3),
-        ((2, 4, 5), 1),
-        ((2, 5, 5), 2),
-    ],
-)
-def test_modpow(inp: tuple[int, int, int], exp: int):
-    assert mr.modpow(*inp) == exp
+@pytest.mark.parametrize("modulus", [*PRIMES])
+def test_modinv(modulus: int, gen_int64: Callable[[], int]):
+    a = gen_int64()
+    if a % modulus == 0:
+        with pytest.raises(ValueError):
+            mr.modinv(a, modulus)
+    else:
+        inv = mr.modinv(a, modulus)
+        mul = (inv * a) % modulus
+        assert mul == 1, f"a = {a}; m = {modulus}; inv = {inv}"
