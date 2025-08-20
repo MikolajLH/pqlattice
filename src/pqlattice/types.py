a69b663db60dba8eed4e1ef9863cb0bc87726651
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-08-20 20:25:23 +0200

    add predicates for type aliases and type validating decorator

--- src/pqlattice/types.py
+++ src/pqlattice/types.py
@@ -1,16 +1,20 @@
 import numpy as np
 from numpy.typing import NDArray
 
-type Vector = NDArray[np.float64 | np.int64]
-type Matrix = NDArray[np.float64 | np.int64]
-type SquareMatrix = NDArray[np.float64 | np.int64]
 
-type VectorFloat = NDArray[np.float64]
-type MatrixFloat = NDArray[np.float64]
-type SquareMatrixFloat = NDArray[np.float64]
+type Float = np.float32
+type Int = np.int64
 
-type VectorInt = NDArray[np.int64]
-type MatrixInt = NDArray[np.int64]
-type SquareMatrixInt = NDArray[np.int64]
+type Vector = NDArray[Float | Int]
+type Matrix = NDArray[Float | Int]
+type SquareMatrix = NDArray[Float | Int]
+
+type VectorFloat = NDArray[Float]
+type MatrixFloat = NDArray[Float]
+type SquareMatrixFloat = NDArray[Float]
+
+type VectorInt = NDArray[Int]
+type MatrixInt = NDArray[Int]
+type SquareMatrixInt = NDArray[Int]
 
 # TODO: Consider using typing.Annotated to define types of values from quotient rings (integer or polynomial)
---
+++ src/pqlattice/types_validator.py
@@ -0,0 +1,103 @@
+from pqlattice.types import *
+from typing import Any, Callable, TypeVar, TypeAliasType
+from inspect import signature
+
+def _is_nparray(obj: Any) -> bool:
+    return isinstance(obj, np.ndarray)
+
+
+def _is_Vector(obj: Any) -> bool:
+    return _is_nparray(obj) and len(obj.shape) == 1
+
+
+def _is_Matrix(obj: Any) -> bool:
+    return _is_nparray(obj) and len(obj.shape) == 2
+
+
+def _is_SquareMatrix(obj: Any) -> bool:
+    return _is_Matrix(obj) and obj.shape[0] == obj.shape[1]
+
+
+def _is_VectorInt(obj: Any) -> bool:
+    return _is_Vector(obj) and obj.dtype == Int
+
+
+def _is_MatrixInt(obj: Any) -> bool:
+    return _is_Matrix(obj) and obj.dtype == Int
+
+
+def _is_SquareMatrixInt(obj: Any) -> bool:
+    return _is_SquareMatrix(obj) and obj.dtype == Int
+
+
+def _is_VectorFloat(obj: Any) -> bool:
+    return _is_Vector(obj) and obj.dtype == Float
+
+
+def _is_MatrixFloat(obj: Any) -> bool:
+    return _is_Matrix(obj) and obj.dtype == Float
+
+
+def _is_SquareMatrixFloat(obj: Any) -> bool:
+    return _is_SquareMatrix(obj) and obj.dtype == Float
+
+
+
+T = TypeVar('T', bound=TypeAliasType)
+def get_predicate_for_alias(type_name: T) -> Callable[[T], bool] | None:
+    
+    # Bare
+    if type_name == Vector:
+        return _is_Vector
+    
+    if type_name == Matrix:
+        return _is_Matrix
+
+    if type_name == SquareMatrix:
+        return _is_SquareMatrix
+    
+    # Ints 
+    if type_name == VectorInt:
+        return _is_VectorInt
+    
+    if type_name == MatrixInt:
+        return _is_MatrixInt
+    
+    if type_name == SquareMatrixInt:
+        return _is_SquareMatrixInt
+    
+
+    # Floats
+    if type_name == VectorFloat:
+        return _is_VectorFloat
+    
+    if type_name == MatrixFloat:
+        return _is_MatrixFloat
+    
+    if type_name == SquareMatrixFloat:
+        return _is_SquareMatrixFloat
+    
+    
+    return None
+
+
+
+def validate_types[**P, T](func: Callable[P, T]) -> Callable[P, T]:
+    def wrapper(*args: P.args, **kwds: P.kwargs) -> T:
+        sig = signature(func)
+        bounded_args = sig.bind(*args, **kwds)
+        bounded_args.apply_defaults()
+        for arg_name, arg_value in bounded_args.arguments.items():
+            if expected_type := func.__annotations__.get(arg_name): # There is a type annotation for the argument
+                pred = get_predicate_for_alias(expected_type)
+                if pred is not None: # type annotations has a predicate to be checked
+                    if not pred(arg_value): # predicate is not fullfilled
+                        raise TypeError(f"in function <{func.__name__}> argument <{arg_name}> with value <{arg_value}> of type <{type(arg_value)}> does not fulfill predicate corresponding to expected type {expected_type}")
+                    else: # predicate is fullfilled
+                        continue
+                else: # there is no predicate for given type annotation, I am not sure what to do, raise exception for now. TODO: change or remove
+                    raise TypeError(f"In function <{func.__name__}>, argument <{arg_name}> with value <{arg_value}>, No predicate defined for annotation <{expected_type}>")
+            else: # No type annotation for argument - raise exception for debugging purposes. TODO: change or remove
+                raise NotImplementedError(f"No annotation in function <{func.__name__}>, for argument <{arg_name}> with value <{arg_value}>")
+        return func(*args, **kwds)
+    return wrapper
