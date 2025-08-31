8e2246860b6733493ed54504da9e5e892b81a7d4
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-09-01 01:16:36 +0200

    add pmodinv function for when the modulus is prime using Euler identity

--- src/pqlattice/integer/__init__.py
+++ src/pqlattice/integer/__init__.py
@@ -1,5 +1,7 @@
+from numpy import ceil, floor, rint
+
 from ._integer import eea
 from ._ModIntRing import ModIntRing as ModIntRing
-from ._modring import mod, modinv, modpow
+from ._modring import mod, modinv, modpow, pmodinv
 
-__all__ = ["eea", "modinv", "modpow", "mod"]
+__all__ = ["eea", "modinv", "modpow", "mod", "floor", "ceil", "rint", "pmodinv"]
