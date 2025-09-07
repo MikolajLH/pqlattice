6e40ab926b6b033527a4312932715576d74b052a
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-09-07 15:25:32 +0200

    write strategy for int polynomials

--- tests/strategies.py
+++ tests/strategies.py
@@ -1,7 +1,9 @@
 from collections.abc import Callable
+from typing import cast
 
 import hypothesis.strategies as st
 import numpy as np
+from hypothesis.extra import numpy as hnp
 from tests import oracle
 
 type DrawFn[T] = Callable[[st.SearchStrategy[T]], T]
@@ -31,19 +33,34 @@ def primes(draw: DrawFn[int], min_value: int = 2, max_bits: int = 128) -> int:
 
 
 @st.composite
-def full_rank_matrices(draw: DrawFn[np.ndarray], n: int, min_value: int = -100, max_value: int = 100):
+def polynomials(draw: DrawFn[int | np.ndarray], min_deg: int = 0, max_deg: int = 20, min_coeff: int = -(10**5), max_coeff: int = 10**5):
     """
     TODO: write docstring
 
     Parameters
     ----------
-    draw : DrawFn[np.ndarray]
+    draw : DrawFn[int | np.ndarray]
         _description_
-    n : int
+    min_deg : int, optional
+        _description_, by default 0
+    max_deg : int, optional
+        _description_, by default 20
+    min_coeff : int, optional
+        _description_, by default -10**5
+    max_coeff : int, optional
+        _description_, by default 10**5
+
+    Returns
+    -------
+    _type_
         _description_
-    min_value : int, optional
-        _description_, by default -100
-    max_value : int, optional
-        _description_, by default 100
     """
-    raise NotImplementedError
+    degree = cast(int, draw(st.integers(min_deg, max_deg)))
+    lead_coeff = cast(int, draw(st.integers(min_coeff, max_coeff).filter(lambda x: x != 0)))
+
+    if degree == 0:
+        return np.array([lead_coeff], dtype=int)
+
+    other_coeffs = draw(hnp.arrays(dtype=int, shape=degree, elements=st.integers(min_coeff, max_coeff)))
+
+    return np.concatenate([other_coeffs, [lead_coeff]])
