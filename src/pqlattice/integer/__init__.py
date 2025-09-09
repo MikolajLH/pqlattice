c4a1d264aaa63bed8af7f44f0da18f96da9b7e1f
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-09-09 19:57:38 +0200

    don't use __init__.py files for now

--- src/pqlattice/integer/__init__.py
+++
@@ -1,7 +0,0 @@
-from numpy import ceil, floor, rint
-
-from ._integer import eea
-from ._ModIntRing import ModIntRing as ModIntRing
-from ._modring import mod, modinv, modpow, pmodinv
-
-__all__ = ["eea", "modinv", "modpow", "mod", "floor", "ceil", "rint", "pmodinv"]
--- src/pqlattice/lattice/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._LLL import GSO, LLL, is_LLL_reduced
-
-__all__ = ["LLL", "GSO", "is_LLL_reduced"]
