b448736959deb5978342399a819deb734dfb5aba
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-09-16 04:20:15 +0200

    write strategies for integer matrices

--- tests/strategies.py
+++ tests/strategies.py
@@ -3,6 +3,7 @@ from typing import cast
 
 import hypothesis.strategies as st
 import numpy as np
+import numpy.typing as npt
 from hypothesis.extra import numpy as hnp
 from tests import oracle
 
@@ -33,7 +34,7 @@ def primes(draw: DrawFn[int], min_value: int = 2, max_bits: int = 128) -> int:
 
 
 @st.composite
-def polynomials(draw: DrawFn[int | np.ndarray], min_deg: int = 0, max_deg: int = 20, min_coeff: int = -(10**5), max_coeff: int = 10**5):
+def polynomials(draw: DrawFn[int | npt.NDArray[np.integer]], min_deg: int = 0, max_deg: int = 20, min_coeff: int = -(10**5), max_coeff: int = 10**5):
     """
     TODO: write docstring
 
@@ -61,6 +62,34 @@ def polynomials(draw: DrawFn[int | np.ndarray], min_deg: int = 0, max_deg: int =
     if degree == 0:
         return np.array([lead_coeff], dtype=int)
 
-    other_coeffs = draw(hnp.arrays(dtype=int, shape=degree, elements=st.integers(min_coeff, max_coeff)))
+    other_coeffs = cast(npt.NDArray[np.integer], draw(hnp.arrays(dtype=int, shape=degree, elements=st.integers(min_coeff, max_coeff))))
 
     return np.concatenate([other_coeffs, [lead_coeff]])
+
+
+@st.composite
+def int_matrices(draw: DrawFn[int | npt.NDArray[np.integer]], min_rows: int = 1, max_rows: int = 10, min_cols: int = 1, max_cols: int = 10, min_value: int = -50, max_value: int = 50):
+    rows = cast(int, draw(st.integers(min_rows, max_rows)))
+    cols = cast(int, draw(st.integers(min_cols, max_cols)))
+
+    matrix = cast(npt.NDArray[np.integer], draw(hnp.arrays(int, (rows, cols), elements=st.integers(min_value, max_value))))
+
+    return matrix
+
+
+@st.composite
+def unimodular_int_matrices(draw: DrawFn[int | npt.NDArray[np.integer]], min_n: int = 1, max_n: int = 10):
+    n = cast(int, draw(st.integers(min_n, max_n)))
+    min_v = -10
+    max_v = 10
+    part_L = cast(npt.NDArray[np.integer], draw(hnp.arrays(int, (n, n), elements=st.integers(min_v, max_v))))
+    part_R = cast(npt.NDArray[np.integer], draw(hnp.arrays(int, (n, n), elements=st.integers(min_v, max_v))))
+
+    diag_L = cast(npt.NDArray[np.integer], draw(hnp.arrays(int, n, elements=st.sampled_from([-1, 1]))))
+
+    diag_R = cast(npt.NDArray[np.integer], draw(hnp.arrays(int, n, elements=st.sampled_from([-1, 1]))))
+
+    L = part_L + np.diag(diag_L)
+    R = part_R + np.diag(diag_R)
+
+    return L @ R
