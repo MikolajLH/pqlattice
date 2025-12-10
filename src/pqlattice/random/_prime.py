daa52c6a97ec0b81f74ddf40d775e85ee0bae177
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-12-10 05:34:11 +0100

    write tests for randprime

--- src/pqlattice/random/_prime.py
+++ src/pqlattice/random/_prime.py
@@ -5,20 +5,28 @@ from ..integer._primality import is_prime
 
 
 def _randprime(a: int, b: int, seed: int | None = None) -> int:
-    def log(n: int, base: float = math.e) -> float:
-        return (n.bit_length() - 1) / math.log2(base)
+    def ilog(n: int, base: float = math.e) -> int:
+        return int((n.bit_length() - 1) / math.log2(base))
 
-    approx_number_of_primes_to_a = 0 if a == 0 else int(a / log(a))
-    approx_number_of_primes_to_b = 0 if b == 0 else int(b / log(b))
-    approx_number_of_primes = approx_number_of_primes_to_b - approx_number_of_primes_to_a
-    prime_proba = approx_number_of_primes / (b - a)
-    number_of_samples = int(math.log(0.001) / math.log(1 - prime_proba)) + 1
+    try:
+        approx_number_of_primes_to_a = 0 if a == 0 else a // ilog(a)
+        approx_number_of_primes_to_b = 0 if b == 0 else b // ilog(b)
+        approx_number_of_primes = approx_number_of_primes_to_b - approx_number_of_primes_to_a
+        prime_proba = approx_number_of_primes / (b - a)
+        number_of_samples = int(math.log(0.001) / math.log(1 - prime_proba)) + 1
+    except ZeroDivisionError:
+        number_of_samples = b - a
+
+    if b - a < 1000:
+        number_of_samples = b - a
 
     random.seed(seed)
-    for _ in range(number_of_samples):
+    for i in range(number_of_samples):
         prime_candidate = random.randint(a, b)
         if is_prime(prime_candidate):
             return prime_candidate
+        if is_prime(a + i):
+            return a + i
 
     raise ValueError(f"Couldn't find a prime number in interval [{a}, {b})")
 
