ba2c8e71ce9373dae638c2670092322bbb25013c
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2026-01-08 02:41:53 +0100

    write show function

--- src/pqlattice/__init__.py
+++ src/pqlattice/__init__.py
@@ -1,6 +1,6 @@
 from fractions import Fraction
 
 from . import integer, lattice, linalg, polynomial, random, typing
-from ._utils import as_integer, as_rational
+from ._utils import as_integer, as_rational, show
 
-__all__ = ["integer", "lattice", "linalg", "polynomial", "random", "typing", "as_integer", "as_rational", "Fraction"]
+__all__ = ["integer", "lattice", "linalg", "polynomial", "random", "typing", "as_integer", "as_rational", "Fraction", "show"]
--- src/pqlattice/typing/_types.py
+++ src/pqlattice/typing/_types.py
@@ -1,3 +1,4 @@
+from fractions import Fraction
 from typing import Any
 
 from numpy.typing import NDArray
@@ -7,3 +8,11 @@ type Array = NDArray[Any]
 type Vector = NDArray[Any]
 type Matrix = NDArray[Any]
 type SquareMatrix = NDArray[Any]
+
+
+def is_rational(a: Array) -> bool:
+    return isinstance(a.flat[0], Fraction)
+
+
+def is_integer(a: Array) -> bool:
+    return isinstance(a.flat[0], int)
