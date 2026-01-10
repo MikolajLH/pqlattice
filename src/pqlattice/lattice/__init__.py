ce944e4139c2f26d72444c1be32978300107e261
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2026-01-10 04:31:01 +0100

    write fast backend that uses fpylll and flint

--- src/pqlattice/lattice/__init__.py
+++ src/pqlattice/lattice/__init__.py
@@ -1,11 +1,27 @@
+from .. import settings
+from ..typing import SquareMatrix, Vector
 from . import embeddings
-from ._bkz import bkz
 from ._cvp import babai_closest_vector, babai_nearest_plane, closest_vector
 from ._gso import gso
-from ._hkz import hkz
 from ._lattice import discriminant, gaussian_heuristic, glr_2dim, hadamard_ratio, rank, volume
-from ._lll import is_lll_reduced, is_size_reduced, lll
-from ._svp import shortest_vector
+from ._lll import is_lll_reduced, is_size_reduced
+
+
+def lll(lattice_basis: SquareMatrix, delta: float = 0.99) -> SquareMatrix:
+    return settings.get_backend().lll(lattice_basis, delta)
+
+
+def bkz(lattice_basis: SquareMatrix, block_size: int = 10, delta: float = 0.99) -> SquareMatrix:
+    return settings.get_backend().bkz(lattice_basis, block_size, delta)
+
+
+def hkz(lattice_basis: SquareMatrix, delta: float = 0.99) -> SquareMatrix:
+    return settings.get_backend().hkz(lattice_basis, delta)
+
+
+def shortest_vector(lattice_basis: SquareMatrix) -> Vector:
+    return settings.get_backend().shortest_vector(lattice_basis)
+
 
 __all__ = [
     "volume",
--- src/pqlattice/linalg/__init__.py
+++ src/pqlattice/linalg/__init__.py
@@ -1,6 +1,13 @@
-from ._linalg import cofactor, cofactor_matrix, det, hnf, left_kernel, left_nullity, minor, rank, right_kernel, right_nullity
+from .. import settings
+from ..typing import Matrix, SquareMatrix
+from ._linalg import cofactor, cofactor_matrix, det, left_kernel, left_nullity, minor, rank, right_kernel, right_nullity
 from ._modint import mod_left_kernel, mod_left_nullity, mod_matinv, mod_ref, mod_right_kernel, mod_right_nullity, mod_rref
 from ._utils import norm, norm2, per_row_norm, per_row_norm2, row_add, row_scale, row_swap
+
+
+def hnf(matrix: SquareMatrix) -> tuple[Matrix, SquareMatrix]:
+    return settings.get_backend().hnf(matrix)
+
 
 __all__ = [
     "hnf",
